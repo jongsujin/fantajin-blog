a793b936f6890af7182440f9a62b2adb036c9319
jongsujin <jjs9836@naver.com>
2025-03-30 23:29:55 +0900

    fix: fs 모듈 수정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // 클라이언트 번들에서 서버 전용 모듈 제외
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      }
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
