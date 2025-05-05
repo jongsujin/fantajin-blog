26607d9706d3d9bdc725f495d3b298880bbcd60b
jongsujin <jjs9836@naver.com>
2025-05-05 15:55:43 +0900

    feat: React 톺아보기 시리즈 재조정 작성

--- src/screens/blog/post-detail/PostDetail.tsx
+++ src/screens/blog/post-detail/PostDetail.tsx
@@ -1,10 +1,6 @@
 'use client'
 
-import {
-  MDXHeadingProps,
-  MDXListProps,
-  PostDetailProps,
-} from '@/src/entities/post/model/types'
+import { PostDetailProps } from '@/src/entities/post/model/types'
 import { format, parseISO } from 'date-fns'
 import { ko } from 'date-fns/locale'
 import { MDXRemote } from 'next-mdx-remote'
@@ -14,53 +10,6 @@ import { MDXImage } from '@/src/shared/ui/MDXImage'
 
 const components = {
   img: MDXImage,
-  h1: ({ children, ...props }: MDXHeadingProps) => (
-    <h1 className="mt-8 mb-4 text-4xl font-bold" {...props}>
-      {children}
-    </h1>
-  ),
-  h2: ({ children, ...props }: MDXHeadingProps) => (
-    <h2 className="mt-6 mb-3 text-3xl font-bold" {...props}>
-      {children}
-    </h2>
-  ),
-  h3: ({ children, ...props }: MDXHeadingProps) => (
-    <h3 className="mt-5 mb-2 text-2xl font-bold" {...props}>
-      {children}
-    </h3>
-  ),
-  p: ({ children, ...props }: MDXHeadingProps) => (
-    <p className="my-4 text-xl leading-relaxed" {...props}>
-      {children}
-    </p>
-  ),
-  ul: ({ children, ...props }: MDXListProps) => (
-    <ul className="my-4 text-xl list-disc pl-6" {...props}>
-      {children}
-    </ul>
-  ),
-  ol: ({
-    children,
-    className,
-    ...props
-  }: React.OlHTMLAttributes<HTMLOListElement>) => (
-    <ol className={`my-4 text-xl list-decimal pl-6 ${className || ''}`} {...props}>
-      {children}
-    </ol>
-  ),
-  pre: ({ children, ...props }: React.HTMLAttributes<HTMLPreElement>) => (
-    <pre className="rounded-lg overflow-auto p-4" {...props}>
-      {children}
-    </pre>
-  ),
-  code: ({ className, children, ...props }: React.HTMLAttributes<HTMLElement>) => {
-    return (
-      <code className={className} {...props}>
-        {children}
-      </code>
-    );
-  },
- 
 }
 
 export default function PostDetail({ post }: PostDetailProps) {
@@ -86,11 +35,7 @@ export default function PostDetail({ post }: PostDetailProps) {
           </div>
           <div>
             {post.content && (
-              <MDXRemote
-                {...post.content}
-                components={components}
-         
-              />
+              <MDXRemote {...post.content} components={components} />
             )}
           </div>
           <Comments slug={post.slug} />
